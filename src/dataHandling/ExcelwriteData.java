745dbba93e00da946d1c1da648f8e4c260e4fbfc
REMAX <rencyrodrigues777@gmai.com>
2023-05-18 13:05:59 -0400

    excel write is changed

--- src/dataHandling/ExcelwriteData.java
+++ src/dataHandling/ExcelwriteData.java
@@ -3,7 +3,10 @@ package dataHandling;
 public class ExcelwriteData {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String path=System.getProperty("user.dir");
+		String excelfilepath="\\Data";
+		String fullpath=path+excelfilepath;
+		System.out.println(fullpath);
 
 	}
 
